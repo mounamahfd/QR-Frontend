a2be9f83aad785ce357bcb0915067820758002d8
mounamahfd <22074@supnum.mr>
2024-12-25 15:58:22 +0000

    Fix asset paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,17 +4,16 @@ let assetPrefix = "";
 let basePath = "";
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split("/")[1]; // Extract the repo name
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
 
 module.exports = {
-  output: "export", // Ensure static export
-  assetPrefix, // Prefix for static assets
-  basePath, // Base path for routing
-  trailingSlash: true, // Ensure all routes have trailing slashes (important for static hosting)
+  output: "export",
+  assetPrefix,
+  basePath,
   images: {
-    unoptimized: true, // Disable image optimization for GitHub Pages
+    unoptimized: true,
   },
 };
