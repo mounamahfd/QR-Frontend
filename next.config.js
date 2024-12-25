6345e27bfcee098e7178530b85d38a55d32daf42
mounamahfd <22074@supnum.mr>
2024-12-25 19:12:06 +0000

    Resolved merge conflicts in next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-=======
-/** @type {import('next').NextConfig} */
->>>>>>> 3da2d1707de07bd4db1a96e780383cd54f47d03b
 const nextConfig = {
   output: "export",
   basePath: "/QR-Frontend",
   assetPrefix: "/QR-Frontend/",
-<<<<<<< HEAD
 };
 
 export default nextConfig;
-=======
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
->>>>>>> 3da2d1707de07bd4db1a96e780383cd54f47d03b
