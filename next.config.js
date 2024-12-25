311536d4d9abec7cba7010a4c6cdc8d46fbd5e01
mounamahfd <22074@supnum.mr>
2024-12-25 19:05:27 +0000

    Your commit message

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split("/")[1]; // Extract the repo name
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
-  output: "export", // Ensure static export
-  assetPrefix, // Prefix for static assets
-  basePath, // Base path for routing
-  trailingSlash: true, // Ensure all routes have trailing slashes (important for static hosting)
-  images: {
-    unoptimized: true, // Disable image optimization for GitHub Pages
-  },
+const nextConfig = {
+  output: "export",
+  basePath: "/QR-Frontend",
+  assetPrefix: "/QR-Frontend/",
 };
+
+export default nextConfig;
