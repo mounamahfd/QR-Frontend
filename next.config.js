ae8fdcd36affcf6c513ff3d2ba35b536ec77839e
mounamahfd <22074@supnum.mr>
2024-12-25 17:50:12 +0000

    Normalize line endings

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  assetPrefix,
-  basePath,
+  basePath: "/QR-Frontend",
+  assetPrefix: "/QR-Frontend/",
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
